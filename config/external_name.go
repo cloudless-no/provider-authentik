0b563a403199a778d1b36ac4ef7541f5e4f30de5
Patrick Lee Scott <pat@patscott.io>
2025-10-09 16:15:08 -0500

    feat: organize external name vs package/ dir structure

--- config/external_name.go
+++ config/external_name.go
@@ -6,29 +6,76 @@ package config
 
 import "github.com/crossplane/upjet/pkg/config"
 
+// Deprecated terraform resources not included:
+// "authentik_property_mapping_google_workspace"
+// "authentik_property_mapping_ldap"
+// "authentik_property_mapping_microsoft_entra"
+// "authentik_property_mapping_rac"
+// "authentik_property_mapping_radius"
+// "authentik_property_mapping_saml"
+// "authentik_property_mapping_scim"
+// "authentik_scope_mapping"
+// "authentik_property_mapping_source_kerberos"
+
 // ExternalNameConfigs contains all external name configurations for this
 // provider.
 var ExternalNameConfigs = map[string]config.ExternalName{
-	// Applications
-	"authentik_application":                   config.IdentifierFromProvider,
-	"authentik_application_entitlement":       config.IdentifierFromProvider,
-	"authentik_outpost":                       config.IdentifierFromProvider,
-	"authentik_provider_google_workspace":     config.IdentifierFromProvider,
-	"authentik_provider_ldap":                 config.IdentifierFromProvider,
-	"authentik_provider_microsoft_entra":      config.IdentifierFromProvider,
-	"authentik_provider_oauth2":               config.IdentifierFromProvider,
-	"authentik_provider_proxy":                config.IdentifierFromProvider,
-	"authentik_provider_rac":                  config.IdentifierFromProvider,
-	"authentik_provider_radius":               config.IdentifierFromProvider,
-	"authentik_provider_saml":                 config.IdentifierFromProvider,
-	"authentik_provider_scim":                 config.IdentifierFromProvider,
-	"authentik_provider_ssf":                  config.IdentifierFromProvider,
-	"authentik_rac_endpoint":                  config.IdentifierFromProvider,
-	"authentik_service_connection_docker":     config.IdentifierFromProvider,
-	"authentik_service_connection_kubernetes": config.IdentifierFromProvider,
-	// Blueprints
+	// applications
+	"authentik_application":             config.IdentifierFromProvider,
+	"authentik_application_entitlement": config.IdentifierFromProvider,
+
+	// blueprints
 	"authentik_blueprint": config.IdentifierFromProvider,
-	// Customization
+
+	// docker
+	"authentik_service_connection_docker": config.IdentifierFromProvider,
+
+	// enterprise
+	"authentik_enterprise_license": config.IdentifierFromProvider,
+
+	// flows
+	"authentik_flow":               config.IdentifierFromProvider,
+	"authentik_flow_stage_binding": config.IdentifierFromProvider,
+
+	// google workspace
+	"authentik_provider_google_workspace":                  config.IdentifierFromProvider,
+	"authentik_property_mapping_provider_google_workspace": config.IdentifierFromProvider,
+
+	// k8s
+	"authentik_service_connection_kubernetes": config.IdentifierFromProvider,
+
+	// kerberos
+	"authentik_source_kerberos": config.IdentifierFromProvider,
+
+	// ldap
+	"authentik_provider_ldap":                config.IdentifierFromProvider,
+	"authentik_property_mapping_source_ldap": config.IdentifierFromProvider,
+	"authentik_source_ldap":                  config.IdentifierFromProvider,
+
+	// microsoftentra
+	"authentik_provider_microsoft_entra":                  config.IdentifierFromProvider,
+	"authentik_property_mapping_provider_microsoft_entra": config.IdentifierFromProvider,
+
+	// notifications
+	"authentik_property_mapping_notification": config.IdentifierFromProvider,
+	"authentik_event_rule":                    config.IdentifierFromProvider,
+	"authentik_event_transport":               config.IdentifierFromProvider,
+
+	// oauth
+	"authentik_property_mapping_source_oauth": config.IdentifierFromProvider,
+	"authentik_source_oauth":                  config.IdentifierFromProvider,
+
+	// oauth2
+	"authentik_provider_oauth2": config.IdentifierFromProvider,
+
+	// outposts
+	"authentik_outpost": config.IdentifierFromProvider,
+
+	// plex
+	"authentik_property_mapping_source_plex": config.IdentifierFromProvider,
+	"authentik_source_plex":                  config.IdentifierFromProvider,
+
+	// policies
 	"authentik_policy_binding":         config.IdentifierFromProvider,
 	"authentik_policy_dummy":           config.IdentifierFromProvider,
 	"authentik_policy_event_matcher":   config.IdentifierFromProvider,
@@ -38,45 +85,43 @@ var ExternalNameConfigs = map[string]config.ExternalName{
 	"authentik_policy_password":        config.IdentifierFromProvider,
 	"authentik_policy_reputation":      config.IdentifierFromProvider,
 	"authentik_policy_unique_password": config.IdentifierFromProvider,
-	// "authentik_property_mapping_google_workspace":          config.IdentifierFromProvider,
-	// "authentik_property_mapping_ldap":                      config.IdentifierFromProvider,
-	// "authentik_property_mapping_microsoft_entra":           config.IdentifierFromProvider,
-	"authentik_property_mapping_notification":              config.IdentifierFromProvider,
-	"authentik_property_mapping_provider_google_workspace": config.IdentifierFromProvider,
-	"authentik_property_mapping_provider_microsoft_entra":  config.IdentifierFromProvider,
-	"authentik_property_mapping_provider_rac":              config.IdentifierFromProvider,
-	"authentik_property_mapping_provider_radius":           config.IdentifierFromProvider,
-	"authentik_property_mapping_provider_saml":             config.IdentifierFromProvider,
-	"authentik_property_mapping_provider_scim":             config.IdentifierFromProvider,
-	"authentik_property_mapping_provider_scope":            config.IdentifierFromProvider,
-	// "authentik_property_mapping_rac":                       config.IdentifierFromProvider,
-	// "authentik_property_mapping_radius":                    config.IdentifierFromProvider,
-	// "authentik_property_mapping_saml":                      config.IdentifierFromProvider,
-	// "authentik_property_mapping_scim":                      config.IdentifierFromProvider,
-	"authentik_property_mapping_source_kerberos": config.IdentifierFromProvider,
-	"authentik_property_mapping_source_ldap":     config.IdentifierFromProvider,
-	"authentik_property_mapping_source_oauth":    config.IdentifierFromProvider,
-	"authentik_property_mapping_source_plex":     config.IdentifierFromProvider,
-	"authentik_property_mapping_source_saml":     config.IdentifierFromProvider,
-	"authentik_property_mapping_source_scim":     config.IdentifierFromProvider,
-	// "authentik_scope_mapping":                              config.IdentifierFromProvider,
-	// Directory
-	"authentik_group":           config.IdentifierFromProvider,
-	"authentik_source_kerberos": config.IdentifierFromProvider,
-	"authentik_source_ldap":     config.IdentifierFromProvider,
-	"authentik_source_oauth":    config.IdentifierFromProvider,
-	"authentik_source_plex":     config.IdentifierFromProvider,
-	"authentik_source_saml":     config.IdentifierFromProvider,
-	"authentik_token":           config.IdentifierFromProvider,
-	"authentik_user":            config.IdentifierFromProvider,
-	// Enterprise
-	"authentik_enterprise_license": config.IdentifierFromProvider,
-	// Events
-	"authentik_event_rule":      config.IdentifierFromProvider,
-	"authentik_event_transport": config.IdentifierFromProvider,
-	// Flows & Stages
-	"authentik_flow":                              config.IdentifierFromProvider,
-	"authentik_flow_stage_binding":                config.IdentifierFromProvider,
+
+	// proxy
+	"authentik_provider_proxy": config.IdentifierFromProvider,
+
+	// rac
+	"authentik_provider_rac":                  config.IdentifierFromProvider,
+	"authentik_rac_endpoint":                  config.IdentifierFromProvider,
+	"authentik_property_mapping_provider_rac": config.IdentifierFromProvider,
+
+	// radius
+	"authentik_provider_radius":                  config.IdentifierFromProvider,
+	"authentik_property_mapping_provider_radius": config.IdentifierFromProvider,
+
+	// rbac
+	"authentik_rbac_initial_permissions": config.IdentifierFromProvider,
+	"authentik_rbac_permission_role":     config.IdentifierFromProvider,
+	"authentik_rbac_permission_user":     config.IdentifierFromProvider,
+	"authentik_rbac_role":                config.IdentifierFromProvider,
+
+	// saml
+	"authentik_provider_saml":                  config.IdentifierFromProvider,
+	"authentik_property_mapping_provider_saml": config.IdentifierFromProvider,
+	"authentik_property_mapping_source_saml":   config.IdentifierFromProvider,
+	"authentik_source_saml":                    config.IdentifierFromProvider,
+
+	// scim
+	"authentik_provider_scim":                  config.IdentifierFromProvider,
+	"authentik_property_mapping_provider_scim": config.IdentifierFromProvider,
+	"authentik_property_mapping_source_scim":   config.IdentifierFromProvider,
+
+	// scope
+	"authentik_property_mapping_provider_scope": config.IdentifierFromProvider,
+
+	// ssf
+	"authentik_provider_ssf": config.IdentifierFromProvider,
+
+	// stages
 	"authentik_stage_authenticator_duo":           config.IdentifierFromProvider,
 	"authentik_stage_authenticator_email":         config.IdentifierFromProvider,
 	"authentik_stage_authenticator_endpoint_gdtc": config.IdentifierFromProvider,
@@ -102,15 +147,16 @@ var ExternalNameConfigs = map[string]config.ExternalName{
 	"authentik_stage_user_login":                  config.IdentifierFromProvider,
 	"authentik_stage_user_logout":                 config.IdentifierFromProvider,
 	"authentik_stage_user_write":                  config.IdentifierFromProvider,
-	// RBAC
-	"authentik_rbac_initial_permissions": config.IdentifierFromProvider,
-	"authentik_rbac_permission_role":     config.IdentifierFromProvider,
-	"authentik_rbac_permission_user":     config.IdentifierFromProvider,
-	"authentik_rbac_role":                config.IdentifierFromProvider,
-	// System
+
+	// system
 	"authentik_brand":                config.IdentifierFromProvider,
 	"authentik_certificate_key_pair": config.IdentifierFromProvider,
 	"authentik_system_settings":      config.IdentifierFromProvider,
+
+	// users
+	"authentik_group": config.IdentifierFromProvider,
+	"authentik_token": config.IdentifierFromProvider,
+	"authentik_user":  config.IdentifierFromProvider,
 }
 
 // ExternalNameConfigurations applies all external name configs listed in the
